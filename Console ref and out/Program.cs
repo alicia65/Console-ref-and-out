c2990cad57a68a4485ca37b0b80ae12c6bc02547
alicia65 <alicia65>
2020-02-12 20:10:46 -0600

    add comment for Out and Ref keywords

--- Console ref and out/Program.cs
+++ Console ref and out/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*Out keyword has ability to carry data away from a method and sends back greater than one value in one direction.
+ * Furthermore, compiler needs value to be assigned to call function.  On the other hand, ref keyword
+ * informs the compiler regarding to object needs to be initialized prior to go through the method. It has two directions. 
+ */
 
 namespace Console_ref_and_out
 {
@@ -18,7 +22,7 @@ namespace Console_ref_and_out
             Console.WriteLine("Enter your last name");
             string lastName = Console.ReadLine();
             int namelength = FullName(name, lastName, out string fullname);
-            Console.WriteLine("Your full name is " fullname + ". It has this many letters: ") + namelength);
+            Console.WriteLine("Your full name is " fullname + ". It has this many letters: ") + namelength;
             FullName(name, lastName, out string fullname);
             Console.WriteLine("Your full name is " + fullname + ". It has this many letters:  " + namelength);
 
