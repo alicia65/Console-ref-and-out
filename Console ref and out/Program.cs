bbc4fffeb9aa0865ed54072c5606742253180381
alicia65 <alicia65>
2020-02-12 19:23:36 -0600

    Add project files.

---
+++ Console ref and out/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_ref_and_out
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name:");
+            string name = Console.ReadLine();
+            string message = MakeGreeting(name);
+            Console.WriteLine(message);
+
+            Console.WriteLine("Enter your last name");
+            string lastName = Console.ReadLine();
+            int namelength = FullName(name, lastName, out string fullname);
+            Console.WriteLine("Your full name is " fullname + ". It has this many letters: ") + namelength);
+            FullName(name, lastName, out string fullname);
+            Console.WriteLine("Your full name is " + fullname + ". It has this many letters:  " + namelength);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+                 
+        }
+
+        static string MakeGreeting(string name) // This method is collapsed to save space
+        {
+            
+        }
+
+        static int FullName( string firstName, string lastName, out string fullname) 
+        {
+            fullname = firstName + " " + lastName;
+            return fullname.Length;
+        }
+    }
+}
